1486490d6c3f4a014f4437bdd49048941900bddf
李光伟 <liguangwei@yonrun.com>
2022-06-10 17:03:55 +0800

    优化

--- source/js/app.js
+++ source/js/app.js
@@ -102,11 +102,37 @@
         });
     };
 
+    const dynamicEvent = function() {
+        let $dynamic = $("#dynamic-content");
+        if ($dynamic.length > 0) {
+            $(".praise").off("click").on("click",function () {
+                let that = this;
+                let id = $(this).data("id");
+                let key = "dynamic-hasPrize" + id;
+                if (sessionStorage.getItem(key)) {
+                    layer.msg("已点赞");
+                    return;
+                }
+
+                $.post("/praiseDynamic/" + id, null, function (resp) {
+                    if (resp.success) {
+                        $(that).find(".praise-num").text(resp.data);
+                        $(that).find(".fa").css("color", "red");
+                        sessionStorage.setItem(key, "y");
+                        layer.msg("点赞成功");
+                    }
+                },"json");
+
+            });
+        }
+    };
+
     $(function() {
         loadLazy();
         navEvent();
         toTop();
         postEvent();
+        dynamicEvent();
     });
 
 })(jQuery, window);
